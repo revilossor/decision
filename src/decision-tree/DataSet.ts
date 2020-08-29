acc138f3ef84d0b3d07870f53abe5426c546017f
Oliver Ross <oliver.ross01@bbc.co.uk>
2020-08-29 15:41:14 +0100

    test

--- src/decision-tree/DataSet.ts
+++ src/decision-tree/DataSet.ts
@@ -1,66 +1,68 @@
-import * as fs from 'fs'
-import * as path from 'path'
+import * as fs from 'fs';
+import * as path from 'path';
 
 export class DataSet {
   public records: string[][]
 
-  constructor(public attributes: string[], ...records: string[][]) {
-    this.records = records
+  constructor (public attributes: string[], ...records: string[][]) {
+    this.records = records;
   }
 
-  public getAttributeIndex(attribute:string): number {
-    const index = this.attributes.indexOf(attribute)
-    if(index < 0) {
-      throw Error(`expected DataSet to contain attribute "${attribute}"`)
+  public getAttributeIndex (attribute:string): number {
+    const index = this.attributes.indexOf(attribute);
+    if (index < 0) {
+      throw Error(`expected DataSet to contain attribute "${attribute}"`);
     }
-    return index
+    return index;
   }
 
   // TODO data file load/parse class...?
-  private static load(filepath: string): Buffer {
+
+  private static load (filepath: string): Buffer {
     try {
-      return fs.readFileSync(path.resolve(filepath))
+      return fs.readFileSync(path.resolve(filepath));
     } catch (e) {
-      throw Error(`there was a problem reading the file: ${e.message}`)
+      throw Error(`there was a problem reading the file: ${e.message}`);
     }
   }
-  private static parse(buffer: Buffer): string[][] {
+
+  private static parse (buffer: Buffer): string[][] {
     try {
-      if(buffer.length === 0) {
-        throw Error('no content')
+      if (buffer.length === 0) {
+        throw Error('no content');
       }
 
-      const content = buffer.toString()
-      const [ attributesRow, ...recordRows ] = content.split('\n')
+      const content = buffer.toString();
+      const [attributesRow, ...recordRows] = content.split('\n');
 
-      const attributes = attributesRow.split(',')
+      const attributes = attributesRow.split(',');
 
       const records = recordRows
         .map(row => row.split(','))
-        .filter(row => row.length !== 1 && row[0] !== '') // allow empty but trim
+        .filter(row => row.length !== 1 && row[0] !== ''); // allow empty but trim
 
-      if(records.length === 0) {
-        throw Error('no records')
+      if (records.length === 0) {
+        throw Error('no records');
       }
 
       records.forEach(row => {
-        if(row.length < attributes.length) {
-          throw Error('missing attribute value')
+        if (row.length < attributes.length) {
+          throw Error('missing attribute value');
         } else if (row.length > attributes.length) {
-          throw Error('extra attribute value')
+          throw Error('extra attribute value');
         }
-      })
+      });
 
-      return [ attributes, ...records ]
+      return [attributes, ...records];
     } catch (e) {
-      throw Error(`there was a problem parsing the file: ${e.message}`)
+      throw Error(`there was a problem parsing the file: ${e.message}`);
     }
   }
 
-  public static fromFilePath(filepath: string): DataSet {
-    const content = DataSet.load(filepath)
-    const [ attributes, ...records ] = DataSet.parse(content)
+  public static fromFilePath (filepath: string): DataSet {
+    const content = DataSet.load(filepath);
+    const [attributes, ...records] = DataSet.parse(content);
 
-    return new DataSet(attributes, ...records)
+    return new DataSet(attributes, ...records);
   }
 }
